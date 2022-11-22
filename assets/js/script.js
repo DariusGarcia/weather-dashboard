626cca8725f78eb606da7284c20512849395dd7b
Darius Garcia <dariusgarcia888@gmail.com>
2022-11-21 21:41:58 -0800

    add api key and URLs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 var apiKey = '46074b9ef56b76a7cf48ee21aaf3a35b'
+var baseURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+var limit = 5
+var geoCodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`
+
+var coordinates = {
+	lat: '',
+	lon: '',
+}
+
+var searchBtn = document.querySelector('.search-btn')
+var inputEl = document.querySelector('.search-input')
