bb94f4f7a7ee52a84e497c5a86636eff8b687e4b
Darius Garcia <dariusgarcia888@gmail.com>
2022-11-22 22:01:54 -0800

    js logic to fetch coordinates/city weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,46 @@
 var apiKey = '46074b9ef56b76a7cf48ee21aaf3a35b'
-var baseURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
 var limit = 5
-var geoCodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`
-
-var coordinates = {
-	lat: '',
-	lon: '',
-}
 
 var searchBtn = document.querySelector('.search-btn')
-var inputEl = document.querySelector('.search-input')
+
+async function fetchCityData(event) {
+	event.preventDefault()
+	var inputEl = document.querySelector('.search-input')
+	var inputValue = inputEl.value
+	var geoCodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=${limit}&appid=${apiKey}`
+
+	await fetch(geoCodingURL)
+		.then((res) => {
+			return res.json()
+		})
+		.then((data) => {
+			console.log(data[0])
+
+			return fetchWeatherData(data[0])
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+}
+
+async function fetchWeatherData(coordinates) {
+	var { lat } = coordinates
+	var { lon } = coordinates
+	var baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat.toFixed(
+		2
+	)}&lon=${lon.toFixed(2)}&appid=${apiKey}`
+	await fetch(baseURL)
+		.then((res) => {
+			return res.json()
+		})
+		.then((data) => {
+			console.log(data)
+			// return data
+		})
+}
+
+function saveSearch(city) {
+	localStorage.setItem(cityName, city)
+}
+
+searchBtn.addEventListener('click', fetchCityData)
