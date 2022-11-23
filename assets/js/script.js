e7ab575d434e97786656f688e287e2ae0c3fabe1
Darius Garcia <dariusgarcia888@gmail.com>
2022-11-22 23:24:46 -0800

    js logic to create weather cards and populate them

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ var apiKey = '46074b9ef56b76a7cf48ee21aaf3a35b'
 var limit = 5
 
 var searchBtn = document.querySelector('.search-btn')
+var cardWrapperEl = document.querySelector('.card-wrapper')
 
+// function to fetch the lon and lat coordinates
 async function fetchCityData(event) {
 	event.preventDefault()
 	var inputEl = document.querySelector('.search-input')
@@ -23,6 +25,7 @@ async function fetchCityData(event) {
 		})
 }
 
+// function to fetch the city's weather data based on coordinate parameters
 async function fetchWeatherData(coordinates) {
 	var { lat } = coordinates
 	var { lon } = coordinates
@@ -35,12 +38,32 @@ async function fetchWeatherData(coordinates) {
 		})
 		.then((data) => {
 			console.log(data)
-			// return data
+			return data
 		})
 }
 
+function populateCards(weatherData) {
+	// create weather cards and populate them with class attributes
+	var cardArticleEl = document.createElement('article')
+	var cardh3El = document.createElement('h3')
+	cardArticleEl.setAttribute('class', 'singleWeatherCard')
+	cardh3El.setAttribute('class', 'card-date')
+	var iconEl = document.createElement('img')
+	iconEl.setAttribute('class', 'icon')
+	var tempEl = document.createElement('p')
+	tempEl.setAttribute('class', 'temperature')
+	var windEl = document.createElement('p')
+	windEl.setAttribute('class', 'wind')
+	var humidityEl = document.createElement('p')
+	humidityEl.setAttribute('class', 'humidity')
+}
+
+populateCards('hi')
+
+// function to save past searched cities to localStorage
 function saveSearch(city) {
 	localStorage.setItem(cityName, city)
 }
 
+// search city button event handler
 searchBtn.addEventListener('click', fetchCityData)
