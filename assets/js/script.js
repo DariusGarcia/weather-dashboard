41fba9563430e41b767686034e8c501bb1896571
Darius Garcia <dariusgarcia888@gmail.com>
2022-11-22 23:33:46 -0800

    display city and state on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var limit = 5
 
 var searchBtn = document.querySelector('.search-btn')
 var cardWrapperEl = document.querySelector('.card-wrapper')
+var cityEl = document.querySelector('.city')
+var stateVar
 
 // function to fetch the lon and lat coordinates
 async function fetchCityData(event) {
@@ -17,7 +19,7 @@ async function fetchCityData(event) {
 		})
 		.then((data) => {
 			console.log(data[0])
-
+			stateVar = data[0].state
 			return fetchWeatherData(data[0])
 		})
 		.catch((err) => {
@@ -36,29 +38,31 @@ async function fetchWeatherData(coordinates) {
 		.then((res) => {
 			return res.json()
 		})
-		.then((data) => {
-			console.log(data)
-			return data
+		.then((weatherData) => {
+			console.log(weatherData)
+			return populateCards(weatherData)
 		})
 }
 
 function populateCards(weatherData) {
 	// create weather cards and populate them with class attributes
-	var cardArticleEl = document.createElement('article')
-	var cardh3El = document.createElement('h3')
-	cardArticleEl.setAttribute('class', 'singleWeatherCard')
-	cardh3El.setAttribute('class', 'card-date')
-	var iconEl = document.createElement('img')
-	iconEl.setAttribute('class', 'icon')
-	var tempEl = document.createElement('p')
-	tempEl.setAttribute('class', 'temperature')
-	var windEl = document.createElement('p')
-	windEl.setAttribute('class', 'wind')
-	var humidityEl = document.createElement('p')
-	humidityEl.setAttribute('class', 'humidity')
+	// for loop for each of the five days of the forecast
+	cityEl.innerHTML = `${weatherData.city.name}, ${stateVar}`
+	for (var i = 0; i < 5; i++) {
+		var cardArticleEl = document.createElement('article')
+		var cardh3El = document.createElement('h3')
+		cardArticleEl.setAttribute('class', 'singleWeatherCard')
+		cardh3El.setAttribute('class', 'card-date')
+		var iconEl = document.createElement('img')
+		iconEl.setAttribute('class', 'icon')
+		var tempEl = document.createElement('p')
+		tempEl.setAttribute('class', 'temperature')
+		var windEl = document.createElement('p')
+		windEl.setAttribute('class', 'wind')
+		var humidityEl = document.createElement('p')
+		humidityEl.setAttribute('class', 'humidity')
+	}
 }
-
-populateCards('hi')
 
 // function to save past searched cities to localStorage
 function saveSearch(city) {
