e7eabc441c7e1fd117802eb088b28db6bc4f6f10
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-06 21:50:52 -0800

    fixed rest of logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-var apiKey = '46074b9ef56b76a7cf48ee21aaf3a35b'
-var limit = 5
+const apiKey = '46074b9ef56b76a7cf48ee21aaf3a35b'
+const limit = 5
 
-var searchBtn = document.querySelector('.search-btn')
-var cardWrapperEl = document.querySelector('.card-wrapper')
-var historyEl = document.querySelector('.searchHistory')
-var cityEl = document.querySelector('.city')
+const searchBtn = document.querySelector('.search-btn')
+const cardWrapperEl = document.querySelector('.card-wrapper')
+const currentWeatherWrapperEl = document.querySelector(
+	'.currentWeather-wrapper'
+)
+const historyEl = document.querySelector('.search-wrapper')
+const cityEl = document.querySelector('.city')
 var stateVar
 
 // function to fetch the lon and lat coordinates
@@ -20,7 +23,8 @@ function fetchCityData(event) {
 		})
 		.then((data) => {
 			stateVar = data[0].state
-			return fetchWeatherData(data[0])
+			fetchCurrentWeather(data[0])
+			fetchWeatherData(data[0])
 		})
 		.catch((err) => {
 			console.log(err)
@@ -50,14 +54,14 @@ function populateCards(weatherData) {
 	// create weather cards and populate them with class attributes
 	// for loop for each of the five days of the forecast
 	cityEl.innerHTML = `${weatherData.city.name}, ${stateVar}`
-	for (var i = 0; i < weatherData.list.length; i += 8) {
+	for (let i = 0; i < weatherData.list.length; i += 8) {
 		// create elements
-		var cardArticleEl = document.createElement('article')
-		var cardh3El = document.createElement('h3')
-		var iconEl = document.createElement('img')
-		var tempEl = document.createElement('p')
-		var windEl = document.createElement('p')
-		var humidityEl = document.createElement('p')
+		const cardArticleEl = document.createElement('article')
+		const cardh3El = document.createElement('h3')
+		const iconEl = document.createElement('img')
+		const tempEl = document.createElement('p')
+		const windEl = document.createElement('p')
+		const humidityEl = document.createElement('p')
 
 		// set attributes
 		cardArticleEl.setAttribute('class', 'singleWeatherCard')
@@ -114,6 +118,65 @@ function saveSearch(city) {
 	}
 }
 
+// function to fetch current weather data
+const fetchCurrentWeather = (coordinates) => {
+	var { lat } = coordinates
+	var { lon } = coordinates
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			displayCurrentWeather(data)
+			console.log('fetchCurrentWeather: ' + data)
+		})
+		.catch((err) => console.error(err))
+}
+
+// function to display the current weather data to <article></article>
+const displayCurrentWeather = (currentWeather) => {
+	currentWeatherWrapperEl.innerHTML = ''
+	// current weather article
+	const currentCityEl = document.querySelector('.current-city')
+	currentCityEl.innerHTML = `${currentWeather.name}, ${stateVar}`
+
+	// create elements
+	const cardArticleEl = document.createElement('article')
+	const cardh3El = document.createElement('h3')
+	const iconEl = document.createElement('img')
+	const tempEl = document.createElement('p')
+	const windEl = document.createElement('p')
+	const humidityEl = document.createElement('p')
+
+	// set attributes
+	cardArticleEl.setAttribute('class', 'currentWeatherCard')
+	cardh3El.setAttribute('class', 'card-date')
+	iconEl.setAttribute('class', 'current-icon')
+	tempEl.setAttribute('class', 'temperature')
+	iconEl.setAttribute(
+		'src',
+		'https://openweathermap.org/img/w/' +
+			currentWeather.weather[0].icon +
+			'.png'
+	)
+
+	windEl.setAttribute('class', 'wind')
+	humidityEl.setAttribute('class', 'humidity')
+
+	// set inner HTML
+	cardh3El.textContent = ''
+	tempEl.textContent = 'Temp: ' + convertTemp(currentWeather.main.temp) + '°F'
+	windEl.textContent = 'Wind: ' + currentWeather.wind.speed + ' MPH'
+	humidityEl.textContent = 'Humidity: ' + currentWeather.main.humidity + '%'
+
+	// append children to parent el
+	cardArticleEl.appendChild(cardh3El)
+	cardArticleEl.appendChild(iconEl)
+	cardArticleEl.appendChild(tempEl)
+	cardArticleEl.appendChild(windEl)
+	cardArticleEl.appendChild(humidityEl)
+	currentWeatherWrapperEl.appendChild(cardArticleEl)
+}
+
 function handleSearchClick(cityListEl) {
 	cardWrapperEl.innerHTML = ''
 	var apiCall = `https://api.openweathermap.org/geo/1.0/direct?q=${cityListEl}&limit=${limit}&units=imperial&appid=${apiKey}`
@@ -123,7 +186,9 @@ function handleSearchClick(cityListEl) {
 		})
 		.then((data) => {
 			stateVar = data[0].state
-			return fetchWeatherData(data[0])
+			fetchCurrentWeather(data[0])
+
+			fetchWeatherData(data[0])
 		})
 		.catch((err) => {
 			console.log(err)
